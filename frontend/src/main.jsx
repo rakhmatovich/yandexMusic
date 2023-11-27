592995220c4626d5f1c9f5a60c78d5e222ec53a8
rakhmatovich <nurmuxammadrahmatov@gmail.com>
2023-11-27 17:51:56 +0500

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './static/index.css'
+import Router from './Router.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Router />,)
